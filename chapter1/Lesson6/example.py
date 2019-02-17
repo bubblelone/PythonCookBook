3bca140e645b5ac3bff816200aefb1ad7e4bf19e
bubblelone@163.com <bubblelone@163.com>
2019-02-17 18:44:59 +0800

    修改

--- chapter1/Lesson6/example.py
+++ chapter1/Lesson6/example.py
@@ -1,5 +1,4 @@
 
-
 """
 问题：实现一个键对应多个值的字典？
 
@@ -7,3 +6,25 @@
 将这多个值放到另外的容器中， 比如列表或者集合里面。
 
 """
+from collections import defaultdict
+d = defaultdict(list)
+d['a'].append(1)
+d['a'].append(2)
+d['b'].append(4)
+print(type(d))
+print(d)
+
+d = defaultdict(set)
+d['a'].add(1)
+d['a'].add(2)
+d['b'].add(4)
+print(type(d))
+print(d)
+
+d = {}
+d.setdefault('a', []).append(1)
+d.setdefault('a', []).append(2)
+d.setdefault('b', []).append(4)
+print(type(d))
+print(d)
+
