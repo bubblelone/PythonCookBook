a82e59586a8c156ebb0bd6b16ba4b17be5765d8b
bubblelone@163.com <bubblelone@163.com>
2019-02-17 08:36:38 +0800

    修改

--- chapter1/Lesson4/example.py
+++ chapter1/Lesson4/example.py
@@ -1,6 +1,27 @@
+
+"""
+问题：怎样从一个集合中获得最大或者最小的N个元素列表？
+
+解决方案：heapq模块有两个函数： nlargest() 和 nsmallest() 可以完美解决这个问题。
+
+"""
+
 import heapq
 
 nums = [1, 8,2,23,7,-4,18,23,42,37,2]
-heapq.heappop(nums)
-heapq.heapify(nums)
-print(nums)
+
+print(heapq.nlargest(3, nums))
+print(heapq.nsmallest(3, nums))
+
+portfolio = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+print(cheap)
+print(expensive)
